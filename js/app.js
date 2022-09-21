ceef81eb4738481a9e2d01dc10ca55d123ccb82c
Anthony Ng'ang'a <ngash.anto@gmail.com>
2022-09-21 09:01:09 +0300

    Created JS logic for the spinner to hide after 5seconds

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
 const button = document.querySelector(".randomize");
 const nameList = document.querySelector(".name-list");
 const displayName = document.querySelector(".name");
+const spinner = document.querySelector(".spinner");
 
 button.addEventListener("click", ()=>{
     const namesArr = nameList.value.split("\n");
     const randomNum = Math.floor(Math.random() * namesArr.length);
     const randomName = namesArr[randomNum];
-    displayName.textContent = randomName;
+    spinner.style.display="block";
+let interval;
+if(!interval){
+    interval = setInterval(()=>{
+        spinner.style.display="none";
+        displayName.textContent = randomName;
+    },5000)
+}
+
+    
     console.log(randomName)
 })
