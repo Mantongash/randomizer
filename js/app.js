7f5e99cac4c512d2a06919bafb5a600b526f7a65
Anthony Ng'ang'a <ngash.anto@gmail.com>
2022-09-21 08:07:35 +0300

    Created markup for the spinner

--- js/app.js
+++ js/app.js
@@ -3,5 +3,9 @@ const nameList = document.querySelector(".name-list");
 const displayName = document.querySelector(".name");
 
 button.addEventListener("click", ()=>{
-    alert("Vepee Vepee")
+    const namesArr = nameList.value.split("\n");
+    const randomNum = Math.floor(Math.random() * namesArr.length);
+    const randomName = namesArr[randomNum];
+    displayName.textContent = randomName;
+    console.log(randomName)
 })
