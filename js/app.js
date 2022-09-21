42dd24c7aeaf2a96a8f4bfec7d5a9b711e5e95de
Anthony Ng'ang'a <ngash.anto@gmail.com>
2022-09-21 03:21:23 +0300

    Attached an event listener to the randomize button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
-const button = document.querySelector(".randomizer");
+const button = document.querySelector(".randomize");
 const nameList = document.querySelector(".name-list");
 const displayName = document.querySelector(".name");
+
+button.addEventListener("click", ()=>{
+    alert("Vepee Vepee")
+})
