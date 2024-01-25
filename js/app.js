50c03669abca9461f750b3a187abdc16d4056855
Anthony Ng'ang'a <ngash.anto@gmail.com>
2024-01-25 04:53:04 +0300

    Fixed parts of the app that were not working

--- js/app.js
+++ js/app.js
@@ -3,19 +3,21 @@ const nameList = document.querySelector(".name-list");
 const displayName = document.querySelector(".name");
 const spinner = document.querySelector(".spinner");
 
-button.addEventListener("click", ()=>{
-    const namesArr = nameList.value.split("\n");
-    const randomNum = Math.floor(Math.random() * namesArr.length);
-    const randomName = namesArr[randomNum];
-    spinner.style.display="block";
-let interval;
-if(!interval){
-    interval = setInterval(()=>{
-        spinner.style.display="none";
-        displayName.textContent = randomName;
-    },5000)
-}
+button.addEventListener("click", () => {
+  const namesArr = nameList.value.split("\n");
+  const randomNum = Math.floor(Math.random() * namesArr.length);
+  const randomName = namesArr[randomNum];
+  displayName.style.display = "none";
+  spinner.style.display = "block";
+  let interval;
+  if (!interval) {
+    interval = setInterval(() => {
+      spinner.style.display = "none";
 
-    
-    console.log(randomName)
-})
+      displayName.style.display = "block";
+    }, 5000);
+    displayName.textContent = "🔥🔥🔥 " + randomName + " 🔥🔥🔥";
+  }
+
+  console.log(randomName);
+});
